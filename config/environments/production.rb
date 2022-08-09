de521a4b8f3d18aea4c73c1467362a6910e58c20
ganesh chachriya <g.chachriya11@gmail.com>
2022-08-09 21:49:12 +0530

    Update production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.assets.compile = true
+  config.webpacker.check_yarn_integrity = false
+
 end
